04289a4040805b3e09f0689e28b7fbc340c5e0ac
benke <info@typomotor.de>
2023-03-06 21:18:26 +0100

    New version 11.5.1

--- ext_emconf.php
+++ ext_emconf.php
@@ -4,14 +4,14 @@ $EM_CONF[$_EXTKEY] = array (
 	'title' => 'Rel nofollow',
 	'description' => 'Adds a rel-nofollow-attribute to extern links. You can exclude URLs by typoscript.',
 	'category' => 'fe',
-	'version' => '10.4.0',
+	'version' => '11.5.1',
 	'state' => 'stable',
 	'author' => 'Peter Benke',
 	'author_email' => 'info@typomotor.de',
 	'author_company' => null,
 	'constraints' =>[
 		'depends' => [
-			'typo3' => '10.4.0-10.4.99',
+			'typo3' => '11.5.0-11.5.99',
 		],
 	],
 );
