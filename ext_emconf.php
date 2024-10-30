e57c850449edfa948bec2d4b85bd4474f0a225af
Peter Benke <pbenke@allplan.com>
2024-10-30 14:10:15 +0100

    TYPO3 v12: main config

--- ext_emconf.php
+++ ext_emconf.php
@@ -4,14 +4,14 @@ $EM_CONF[$_EXTKEY] = array (
 	'title' => 'Rel nofollow',
 	'description' => 'Adds a rel-nofollow-attribute to extern links. You can exclude URLs by typoscript.',
 	'category' => 'fe',
-	'version' => '11.5.1',
+	'version' => '12.4.0',
 	'state' => 'stable',
 	'author' => 'Peter Benke',
 	'author_email' => 'info@typomotor.de',
 	'author_company' => null,
 	'constraints' =>[
 		'depends' => [
-			'typo3' => '11.5.0-11.5.99',
+            'typo3' => '12.4.0-12.4.99',
 		],
 	],
 );
