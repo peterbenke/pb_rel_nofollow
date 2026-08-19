6d371df1246ccba170185f723b653f934a129767
Albrecht Köhnlein <ak@koehnlein.eu>
2026-08-19 18:06:43 +0200

    Allow to be installed with TYPO3 14 LTS

--- ext_emconf.php
+++ ext_emconf.php
@@ -11,7 +11,7 @@ $EM_CONF[$_EXTKEY] =  [
 	'author_company' => null,
 	'constraints' =>[
 		'depends' => [
-            'typo3' => '13.4.0-13.4.99',
+            'typo3' => '13.4.0-14.3.99',
 		],
 	],
 ];
